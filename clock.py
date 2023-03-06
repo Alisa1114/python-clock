34672f8ed485799d9462882d4f05edd43d8d8f10
Alisa1114 <kafuchino0410@gmail.com>
2023-03-06 20:44:27 +0800

    init git repo

---
+++ clock.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+import time 
+
+def main():
+    def tick():
+        now = time.strftime('%H:%M:%S')
+        label.config(text=now)
+        label.after(200, tick)
+        
+    window = tk.Tk()
+    window.title("Python-Clock")
+    window['width'] = window.winfo_screenwidth()
+    window['height'] = window.winfo_screenheight()
+
+    window.configure(bg='black')
+    window.resizable(0, 0)
+    
+    label = tk.Label(window, text='00:00:00', font=('Arial', 250, 'bold'), fg='white', bg='black')
+    label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+    tick()
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
